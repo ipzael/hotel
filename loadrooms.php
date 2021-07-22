2bb450ff2d4f04190c16fa798ac4141a3e82d304
ipzael <45858086+ipzael@users.noreply.github.com>
2021-07-22 05:29:54 -0500

    Update loadrooms.php

--- loadrooms.php
+++ loadrooms.php
@@ -1,23 +1,22 @@
 <?php
-    while ($mostrar = mysqli_fetch_array($result)) {
-            
+while ($mostrar = mysqli_fetch_array($result)) {
         $RoomDescription = $mostrar['descripcion'];
-	$RoomNumber = $mostrar['habitacion'];
-	$RoomStatusID = $mostrar['room_statusID'];
-
-	echo "<script>";
+        $RoomNumber = $mostrar['habitacion'];
+        $RoomStatusID = $mostrar['room_statusID'];
+        $RoomType = $mostrar['room_desc'];
+        echo "<script>";
         echo "$(document).ready(function() {";
-        echo "var X = '".$RoomDescription."';";
-        echo "var Y = '".$RoomStatusID."';";
-	echo "var Room = new Object();";
-	echo "Room.Number = '".$RoomNumber."';";
+        echo "var X = '" . $RoomDescription . "';";
+        echo "var Y = '" . $RoomStatusID . "';";
+        echo "var Room = new Object();";
+        echo "Room.Number = '" . $RoomNumber . "';";
         echo "Room.ServicesString = X.replace('HABITACION CON ', '');";
-	echo "Room.HotWater = X.includes('AGUA CALIENTE');";
+        echo "Room.HotWater = X.includes('AGUA CALIENTE');";
         echo "Room.TV = X.includes('TV');";
         echo "Room.WiFi = X.includes('WIFI');";
-	echo "Room.Status = '".$RoomStatusID."';";
-	echo "Rooms.push(Room);";
+        echo "Room.Type = '" . $RoomType . "';";
+        echo "Room.Status = '" . $RoomStatusID . "';";
+        echo "Rooms.push(Room);";
         echo  "});";
         echo "</script>";
-    }
-?>
+}
